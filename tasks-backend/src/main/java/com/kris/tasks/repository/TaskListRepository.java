a622274a418a6dbba44f77034e69ebb027a840a6
Kris <emineo@abv.bg>
2024-12-20 13:04:01 +0200

    add repositories

---
+++ tasks-backend/src/main/java/com/kris/tasks/repository/TaskListRepository.java
@@ -0,0 +1,11 @@
+package com.kris.tasks.repository;
+
+import com.kris.tasks.entity.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+}
---
+++ tasks-backend/src/main/java/com/kris/tasks/repository/TaskRepository.java
@@ -0,0 +1,17 @@
+package com.kris.tasks.repository;
+
+import com.kris.tasks.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+    List<Task> findByTaskListId(UUID taskListId);
+
+    Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
+}
